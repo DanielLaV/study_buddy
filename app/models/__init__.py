ab0240e8b72d0eeec8cb869e927675a5f9180340
Sophia <sophiatbui@gmail.com>
2022-02-05 13:00:51 -0700

    updated models __init__ imports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,9 @@
 from .db import db
 from .user import User
+from .cards import Card
+from .comments import Comment
+from .decks import Deck
+from .tags import Tag
+from user_study_card import UserStudyCard
+from user_study_deck import UserStudyDeck
+

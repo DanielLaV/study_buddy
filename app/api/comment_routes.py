cfce48604825e92cd7157ee452f9ef1c6f461bad
cat-friend <denisepli@gmail.com>
2022-02-05 14:41:15 -0500

    completed basic api routes

---
+++ app/api/comment_routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify, session, request
+from app.models import Comment, db
+from app.forms import LoginForm
+from app.forms import SignUpForm
+from flask_login import current_user, login_user, logout_user, login_required
+
+comment_routes = Blueprint('comments', __name__)
+
+@comment_routes.route('/', methods=['GET'])
+def main():
+    """
+    insert function description
+    """
+    return "you are in /api/comments!"

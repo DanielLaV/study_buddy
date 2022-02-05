48da29c9f0e36035890b9bb512468590e72f170e
Sophia <sophiatbui@gmail.com>
2022-02-05 13:04:26 -0700

    fixed line 7/8 to add .

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,6 +4,5 @@ from .cards import Card
 from .comments import Comment
 from .decks import Deck
 from .tags import Tag
-from user_study_card import UserStudyCard
-from user_study_deck import UserStudyDeck
-
+from .user_study_card import UserStudyCard
+from .user_study_deck import UserStudyDeck

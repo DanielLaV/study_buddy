c4e59f09bd15e55919f1f17eb74d2de8917eed40
cat-friend <denisepli@gmail.com>
2022-02-07 13:04:13 -0500

    completed thunk reducers, store state for cards API

--- react-app/src/store/cards.js
+++ react-app/src/store/cards.js
@@ -1,6 +1,7 @@
 const LOAD = 'studybuddy/card/LOAD'
 const ADD_CARD = 'studybuddy/card/ADD_CARD'
 const DELETE_CARD = 'studybuddy/card/DELETE_CARD'
+const DELETE_DECK_CARDS = 'studybuddy/cards/DELETE_DECK_CARDS'
 
 const load = cards => ({
     type: LOAD,
@@ -17,6 +18,11 @@ const deleteOneCard = card => ({
     card
 });
 
+const deleteAllDeckCards = cards => ({
+    type: DELETE_DECK_CARDS,
+    cards
+})
+
 export const getDeckCards = (deckId) => async (dispatch) => {
     const response = await fetch(`/api/decks/${deckId}/cards/`, {
         headers: {
@@ -30,7 +36,7 @@ export const getDeckCards = (deckId) => async (dispatch) => {
     }
 }
 
-export const getOneCard = () => async (dispatch) => {
+export const getOneCard = (id) => async (dispatch) => {
     const response = await fetch(`/api/cards/${id}`, {
         headers: {
             "Content-Type": "application/json"
@@ -62,4 +68,48 @@ export const deleteCard = (id) => async (dispatch) => {
     else return delCard.json();
 }
 
-export 
+export const createCard = (payload) => async (dispatch) => {
+    const response = await fetch("/api/cards", {
+        method: 'POST', body: JSON.stringify(payload)
+    });
+    const card = await response.json();
+    if (response.ok) {
+        dispatch(addOneCard(card));
+    }
+    return card
+}
+
+const cardsReducer = (state = {}, action) => {
+    switch (action.type) {
+        case LOAD: {
+            const allCards = {};
+            action.cards.forEach((card) => {
+                allCards[card.id] = card;
+            });
+            return { ...allCards, ...state }
+        }
+        case ADD_CARD: {
+            const newCard = { ...state };
+            if (!state[action.card.id]) {
+                newCard[action.card.id] = action.card
+                return { ...newCard }
+            }
+            return newCard;
+        }
+        case DELETE_CARD: {
+            const allCards = { ...state };
+            delete allCards[action.card.id];
+            return allCards;
+        }
+        case DELETE_DECK_CARDS: {
+            const allCards = { ...state };
+            action.cards.forEach((card) => {
+                delete allCards[card.id];
+            });
+            return allCards;
+        }
+        default: return state;
+    }
+}
+
+export default cardsReducer

67a343337a8a54f0d830b309b8355437bd24f2b4
Sophia <sophiatbui@gmail.com>
2022-02-07 22:09:45 -0700

    fixed naming convention for ids and add skeleton for post route

--- app/api/userstudydeck_routes.py
+++ app/api/userstudydeck_routes.py
@@ -1,16 +1,23 @@
 from flask import Blueprint, jsonify, session, request
-from app.models import UserStudyDeck, db
-from app.forms import LoginForm, SignUpForm
-from flask_login import current_user, login_user, logout_user, login_required
+from app.models import UserStudyDeck, Deck
+from flask_login import login_required
 
 userstudydeck_routes = Blueprint('user-study-decks', __name__)
 
-@userstudydeck_routes.route('<int:userId>', methods=['GET'])
+@userstudydeck_routes.route('<int:user_id>', methods=['GET'])
 @login_required
-def study_list(userId):
+def study_list(user_id):
     """
     return all of the user's decks in study list
     """
-    study_decks = UserStudyDeck.query.filter(UserStudyDeck.user_id == userId).join(Deck).all()
+    study_decks = UserStudyDeck.query.filter(UserStudyDeck.user_id == user_id).join(Deck).all()
 
     return {"study_decks": [study_deck.to_dict() for study_deck in study_decks]}
+
+@userstudydeck_routes.route('<int:deck_id>', methods=['POST'])
+@login_required
+def add_to_study_list(deck_id):
+    """
+    POST route to add a deck to the study list
+    """
+    return ''

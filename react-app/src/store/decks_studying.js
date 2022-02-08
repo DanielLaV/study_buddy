649253a1a0e8e170e1789e36a1997935caed7377
Sophia <sophiatbui@gmail.com>
2022-02-07 17:53:11 -0700

    fixed reducer code

--- react-app/src/store/decks_studying.js
+++ react-app/src/store/decks_studying.js
@@ -20,17 +20,14 @@ export const getStudyDecks = (userId) => async (dispatch) => {
 	}
 };
 const initialState = {};
-//  [ {'id': 1, 'user_id': 1, 'deck_id': 1, 'toStudy': True,
-//   'decks': {'id': 1, 'title': 'Thunks', 'description': 'Taking a look at React thunks'}},
-//  {'id': 2, 'user_id': 1, 'deck_id': 2, 'toStudy': True,
-//  'decks': {'id': 2, 'title': 'Memoization', 'description': 'No one knows what it is'}} ]}
+
 const studyDecksReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case LOAD_STUDY_DECKS: {
 			const newState = Object.assign({}, state);
+            console.log(action.payload.study_decks)
 			action.payload.study_decks.forEach((studyDeck) => {
-                console.log('---------------', Object.values (studyDeck))
-				newState[studyDeck.id] = Object.values(studyDeck)[1];
+				newState[studyDeck.id] = studyDeck.decks
 			});
 			return newState;
 		}

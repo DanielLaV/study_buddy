a5b1428f20ad8733196d3511b9f1ed8f949638fa
DanielLaV <djjlav@gmail.com>
2022-02-10 15:05:12 -0600

    added history.push so when a deck is deleted the user is sent back to /decks

--- react-app/src/components/DeleteDeckModal/DeleteDeckForm.js
+++ react-app/src/components/DeleteDeckModal/DeleteDeckForm.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { useDispatch } from 'react-redux';
 import * as deckActions from "../../store/decks";
+import { useHistory } from 'react-router-dom';
 
 
 
 function DeleteDeckForm({ payload, deck }) {
     const dispatch = useDispatch();
+    const history = useHistory();
     const setShowModal = payload;
 
     const submitDelete = () => {
         dispatch(deckActions.deleteDeck(deck.id))
         setShowModal(false)
+        history.push('/decks')
     }
 
     return (<><h2>Are you sure you want to delete this deck?</h2>

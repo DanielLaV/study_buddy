d70350ba4fbe2ac5e4656ba645c18bb99c928780
DanielLaV <djjlav@gmail.com>
2022-02-11 15:53:18 -0600

    removed global box-sizing and added styling for AddCardForm

--- react-app/src/components/AddCardFormModal/AddCardForm.js
+++ react-app/src/components/AddCardFormModal/AddCardForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import * as cardActions from "../../store/cards";
 import { useParams } from "react-router-dom";
+import './AddCardForm.css';
 
 function AddCardForm({ payload }) {
   const dispatch = useDispatch();
@@ -37,54 +38,60 @@ function AddCardForm({ payload }) {
   };
 
   return (
-    <div className="card-form">
+    <div className="form-container">
       <h2>
         {success}
       </h2>
-      <h2>Preview</h2>
-      <h3>Front:</h3>
-      <div>{front}</div>
-      <h3>Back:</h3>
-      <div>{back}</div>
+      <div className="preview">
+        <h2 className="preview-title">Preview</h2>
+        <h3>Front:</h3>
+        <div className="preview-text">{front}</div>
+        <h3>Back:</h3>
+        <div className="preview-text">{back}</div>
+      </div>
       <ul className="error-list">
         {errors.map((error, idx) => (
           <li key={idx} className="errors">{error}</li>
         ))}
       </ul>
-      <form onSubmit={handleSubmit} className="">
-        <textarea
-          id="front"
-          type="text"
-          value={front}
-          onChange={(e) => setFront(e.target.value)}
-          required
-          placeholder="Enter what you want to appear on the front of the card. The front can be something like a question, concept, or vocabulary word."
-          className=""
-        />
-        <textarea
-          id="back"
-          type="text"
-          value={back}
-          onChange={(e) => setBack(e.target.value)}
-          required
-          placeholder="Enter what you want to appear on the back of the card. This would be the answer or response to the prompt on the front of the card."
-          className=""
-        />
-        <button
-          type="submit"
-          className="">
-          Add Card!
-        </button>
-        <button
-          type="button"
-          className=""
-          onClick={(e) => {
-            setShowModal(false);
-            setFront("");
-            setBack("");
-          }}>
-          Cancel
-        </button>
+      <form className='form' onSubmit={handleSubmit} className="">
+        <div className="text-container">
+          <textarea
+            id="front"
+            type="text"
+            value={front}
+            onChange={(e) => setFront(e.target.value)}
+            required
+            placeholder="Enter what you want to appear on the front of the card. The front can be something like a question, concept, or vocabulary word."
+            className="input"
+          />
+          <textarea
+            id="back"
+            type="text"
+            value={back}
+            onChange={(e) => setBack(e.target.value)}
+            required
+            placeholder="Enter what you want to appear on the back of the card. This would be the answer or response to the prompt on the front of the card."
+            className="input"
+          />
+        </div>
+        <div className="form-button-container">
+          <button
+            type="submit"
+            className="form-button">
+            Add Card!
+          </button>
+          <button
+            type="button"
+            className="form-button"
+            onClick={(e) => {
+              setShowModal(false);
+              setFront("");
+              setBack("");
+            }}>
+            Cancel
+          </button>
+        </div>
       </form>
     </div>
   );

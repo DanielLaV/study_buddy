a677911a22c4b0d8cc58d190fb13c1c4d970d7a4
cat-friend <denisepli@gmail.com>
2022-02-10 20:33:14 -0500

    added favicon, logic for searching

---
+++ react-app/src/components/SearchForm/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import * as searchActions from "../../store/cards";
+import { Link } from 'react-router-dom'
+function SearchForm() {
+    const dispatch = useDispatch();
+    const [query, setQuery] = useState("");
+    const [errors, setErrors] = useState([]);
+    const user = useSelector(state => state.session.user)
+    const [inputType, setInputType] = useState("search")
+    const [success, setSuccess] = useState("");
+
+    const enterSubmit = (e) => {
+        const key = e.code;
+        if (key === 'Enter') {
+            handleSubmit();
+        }
+    }
+    const handleSubmit = () => {
+        setErrors([]);
+        const payload = {
+            query,
+            is_logged_in: Boolean(user)
+        }
+        console.log("success!")
+        setSuccess("Searching!")
+        setInputType("hidden")
+        // return dispatch(cardActions.createCard(payload))
+        //     .then(
+        //         (response) => {
+        //             if (response.errors) {
+        //                 setErrors(response.errors)
+        //                 return
+        //             }
+        //             setShowSearchForm(false)
+        //             setSuccess("Searching!");
+        //             setTimeout(() => {
+        //                 // setShowModal(false); history.push?
+        //             }, 1500);
+        //         }
+        //     );
+    };
+    return (
+        <>
+            <div className="search">
+                <input
+                    type={inputType}
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    required
+                    placeholder="SEARCH"
+                    onKeyPress={(e) => enterSubmit(e)}
+                />
+                <Link to="#"><i class="fa-solid fa-magnifying-glass" onClick={handleSubmit}></i></Link>
+            </div>
+        </>
+    )
+}
+
+export default SearchForm

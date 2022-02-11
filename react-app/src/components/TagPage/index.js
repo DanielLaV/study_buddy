c17019a52983dcc33aeafb5b57f5a49c589d7719
Kreston <kreston.cm@gmail.com>
2022-02-11 17:37:40 -0600

    Tags page css added

--- react-app/src/components/TagPage/index.js
+++ react-app/src/components/TagPage/index.js
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom';
 import * as tagActions from '../../store/tags'
 import Deck from '../DecksPage/Deck'
+import AddDeckFormModal from '../AddDeckModal';
+import { NavLink } from 'react-router-dom';
 
 function TagPage() {
     const dispatch = useDispatch()
@@ -16,11 +18,30 @@ function TagPage() {
     }, [dispatch, tagId])
 
     return (
-        <div className='decks-by-tag'>
-            <h1>Decks with tag "{tag?.name}"</h1>
-            {decks?.map(deck => <div key={deck.id}> <Deck deck={deck} /> </div>)}
+        <div className='browsePageContainer'>
+            <div className="browseDecks">
+                <div className="browseDecksTitleContainer">
+                    <h1 className="browseDecksTitle">Decks with tag "{tag?.name}"</h1>
+                </div>
+                <div className='deckDisplay'>
+                    <div className='allDecks'>
+                        {decks?.map(deck =>
+                            <NavLink className="eachDeck" to={`/decks/${deck.id}`} key={deck.id}>
+                                <Deck deck={deck} />
+                            </NavLink>)}
+                    </div>
+                </div>
+              
+            </div>
+
+
+
+            {/* {decks?.map(deck => <div key={deck.id}> <Deck deck={deck} studyDecks={studyDecks}/> </div>)} */}
         </div>
     )
+
+
+
 }
 
 export default TagPage;

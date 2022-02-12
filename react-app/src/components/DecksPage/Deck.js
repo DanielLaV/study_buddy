74f9b44983e0c9853e1051104abd11662d7b961b
cat-friend <denisepli@gmail.com>
2022-02-12 15:00:32 -0500

    fixed styling and buttanz for

--- react-app/src/components/DecksPage/Deck.js
+++ react-app/src/components/DecksPage/Deck.js
@@ -1,12 +1,10 @@
 import './DecksPage.css'
 import { useSelector } from 'react-redux';
-import EditDeckFormModal from '../EditDeckModal';
-import DeleteDeckFormModal from '../DeleteDeckModal';
 import DeleteFromSLButton from '../DeleteFromStudyListModal';
 import AddToStudyList from '../AddToStudyList'
 import '../Card/card.css'
 
-function Deck({ deck, studyDecks=[] }) {
+function Deck({ deck, studyDecks = [] }) {
 
     const user = useSelector(state => state.session.user.id)
     const isOwner = user === deck.user_id;
@@ -16,13 +14,8 @@ function Deck({ deck, studyDecks=[] }) {
             <p className='title'>{deck.title}</p>
             {/* <p className='deckDesc'>{deck.description}</p> */}
             {!isStudying &&
-            <AddToStudyList deck_id={deck.id} user_id={user}/>}
-            {isStudying && <DeleteFromSLButton deck_id={deck.id} user_id={user}/>}
-            {isOwner &&
-            <div className='editDeleteButtons'>
-                <EditDeckFormModal deck={deck} />
-                <DeleteDeckFormModal deck={deck} />
-            </div>}
+                <AddToStudyList deck_id={deck.id} user_id={user} />}
+            {isStudying && <DeleteFromSLButton deck_id={deck.id} user_id={user} />}
         </div>
     )
 }

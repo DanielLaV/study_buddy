4dc11ddc08abcaec25ab7776a6389dde5c073c44
DanielLaV <djjlav@gmail.com>
2022-02-11 19:19:23 -0600

    fixed problem with delete card and deck propagating

--- react-app/src/components/DeleteCardFormModal/index.js
+++ react-app/src/components/DeleteCardFormModal/index.js
@@ -6,10 +6,15 @@ import '../Card/card.css'
 function DeleteCardFormModal({card}) {
   const [showModal, setShowModal] = useState(false);
 
+  const onSubmit = e => {
+    e.stopPropagation();
+    setShowModal(true);
+  }
+
   return (
     <>
-      <input type="image" className='trashCard' src="/trash.png" alt="text" onClick={() => setShowModal(true)} to="#" ></input>
-      
+      <input type="image" className='trashCard' src="/trash.png" alt="text" onClick={onSubmit} to="#" ></input>
+
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <DeleteCardForm setShowModal={setShowModal} card={card}/>

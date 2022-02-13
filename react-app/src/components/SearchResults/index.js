c5af34d62ce082538395137452938ee4ebe3a032
cat-friend <denisepli@gmail.com>
2022-02-12 20:03:42 -0500

    fixed issue with search results page

--- react-app/src/components/SearchResults/index.js
+++ react-app/src/components/SearchResults/index.js
@@ -1,8 +1,10 @@
 import { getResults } from "../../store/search";
-import { useLocation } from 'react-router-dom';
+import { useLocation, NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import * as studyDeckActions from "../../store/decks_studying";
+import Deck from "../DecksPage/Deck";
+import SearchCardBodyModal from "../SearchCardBodyModal";
 
 function SearchResults() {
     const dispatch = useDispatch();
@@ -37,58 +39,49 @@ function SearchResults() {
         return studyDecks
     })
 
-    let results
     if (!query) {
-        results = <div className="">Please use the search bar to search the database!</div>
-    }
-    else {
-        results = [results, (
-            <div className="">
-                <ul className="error-list">
-                    {errors.map((error, idx) => (
-                        <li key={idx} className="errors">{error}</li>
-                    ))}
-                </ul>
-            </div>)]
+        return <div className="">Please use the search bar to search the database!</div>
     }
     if (hasResults) {
-        results = [results, (
-            <div className='browsePageContainer'>
-
-                <div className="browseDecks" >
-                    <div className="browseDecksTitleContainer">
-
-                        <h1 className="browseDecksTitle">Decks that Contain "{`${query}`}"</h1>
+        return (<div className='browsePageContainer'>
+            <div className="browseDecks" >
+                <div className="browseDecksTitleContainer">
+                    <h1 className="browseDecksTitle">Decks that Contain "{`${query}`}"</h1>
+                </div>
+                <div className="deckDisplay" >
+                    <div className='allDecks'>
+                        {decks?.map(deck =>
+                            <NavLink className="eachDeck" to={`/decks/${deck.id}`} key={deck.id}>
+                                <Deck deck={deck} studyDecks={studyDecks} />
+                            </NavLink>)}
                     </div>
-                    <div className="deckDisplay" >
-
-                        <div className='allDecks'>
-                            {decks?.map(deck =>
-                                <NavLink className="eachDeck" to={`/decks/${deck.id}`} key={deck.id}>
-                                    <Deck deck={deck} studyDecks={studyDecks} />
-                                </NavLink>)}
-                        </div>
-                    </div >
-                    <div className="browseDecksTitleContainer">
-                        <h1 className="browseDecksTitle">Cards that Contain "{`${query}`}"</h1>
-                    </div>
-                    <div className="deckDisplay" style={{ marginBottom: '55px' }}>
-                        <div className='allDecks'>
-                            {cards?.map((card) => {
-                                return (<div className="eachDeck">
-                                    <SearchCardBodyModal card={card} key={card.id} />
-                                    {/* <CardBodyModal card={card} key={card.id} /> */}
-                                </div>
-                                )
-                            })}
-                        </div>
+                </div >
+                <div className="browseDecksTitleContainer">
+                    <h1 className="browseDecksTitle">Cards that Contain "{`${query}`}"</h1>
+                </div>
+                <div className="deckDisplay" style={{ marginBottom: '55px' }}>
+                    <div className='allDecks'>
+                        {cards?.map((card) => {
+                            return (<div className="eachDeck">
+                                <SearchCardBodyModal card={card} key={card.id} />
+                            </div>
+                            )
+                        })}
                     </div>
                 </div>
             </div>
-        )]
+        </div>)
     }
-    return results
+    else return (
+        <div className='browsePageContainer'>
+            <div className="browseDecks" >
+                <div className="browseDecksTitleContainer">
+                    {errors.map((error, idx) => (
+                        <h1 className="browseDecksTitle" key={idx} className="errors">{error}</h1>
+                    ))}
+                </div>
+            </div>
+        </div>)
 }
-
 
 export default SearchResults
